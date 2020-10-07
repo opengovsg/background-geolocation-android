3777b154a08ec08c60f357a43d2c30be32d0292c
Christabel Neo <christabel@data.gov.sg>
2020-10-08 03:41:10 +0800

    fix: allow import of react native packages

--- src/main/java/com/marianhello/bgloc/messaging/LocationAlarmMessagingService.java
+++ src/main/java/com/marianhello/bgloc/messaging/LocationAlarmMessagingService.java
@@ -1,15 +1,16 @@
 package com.marianhello.bgloc.messaging;
 
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.Context;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.facebook.react.HeadlessJsTaskService;
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
 
-import io.invertase.firebase.Utils;
+import com.marianhello.bgloc.messaging.Utils;
 
 public class LocationAlarmMessagingService {
   private static final String TAG = "LocationAlarmMessagingService";
@@ -17,11 +18,13 @@ public class LocationAlarmMessagingService {
   public static final String FOREGROUND_MESSAGE_EVENT = "foreground-location-alarm";
 
   // instance of our receiver to listen from foreground service timer
-  private LocalBroadcastReceiver mLocalBroadcastReceiver;
+  private LocalTimedBroadcastReceiver mLocalBroadcastReceiver;
+  private Context mContext;
 
   public LocationAlarmMessagingService(Context context) {
     Log.d(TAG, "Creating timed broadcast receiver");
     this.mLocalBroadcastReceiver = new LocalTimedBroadcastReceiver();
+    this.mContext = context;
     LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
     localBroadcastManager.registerReceiver(mLocalBroadcastReceiver, new IntentFilter("my-custom-event"));
   }
@@ -32,24 +35,24 @@ public class LocationAlarmMessagingService {
       Log.d(TAG, "Timed alarm broadcast received");
 
       // If the app is in the foreground we send it to the Messaging module
-      if (Utils.isAppInForeground(this.getApplicationContext())) {
+      if (Utils.isAppInForeground(mContext)) {
         Intent messagingEvent = new Intent(FOREGROUND_MESSAGE_EVENT);
         messagingEvent.putExtra("my-extra-data", "that's it");
         // Broadcast it so it is only available to the RN Application
         LocalBroadcastManager
-          .getInstance(this)
+          .getInstance(mContext)
           .sendBroadcast(messagingEvent);
       } else {
         try {
           // If the app is in the background we send it to the Headless JS Service
           Intent headlessIntent = new Intent(
-            this.getApplicationContext(),
+            mContext,
             LocationAlarmBackgroundMessagingService.class
           );
           headlessIntent.putExtra("my-extra-data", "that's it");
-          ComponentName name = this.getApplicationContext().startService(headlessIntent);
+          ComponentName name = mContext.startService(headlessIntent);
           if (name != null) {
-            HeadlessJsTaskService.acquireWakeLockNow(this.getApplicationContext());
+            HeadlessJsTaskService.acquireWakeLockNow(mContext);
           }
         } catch (IllegalStateException ex) {
           Log.e(
